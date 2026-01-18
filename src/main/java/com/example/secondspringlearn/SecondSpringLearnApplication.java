ffc0ae21fdd681ddf9ad2ff198c8c38fe42b42bb
Valentyn_Koval <J.K.play2015@gmail.com>
2026-01-18 18:55:48 +0200

    Initial commit

---
+++ src/main/java/com/example/secondspringlearn/SecondSpringLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondspringlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondSpringLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/secondspringlearn/SecondSpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondspringlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpringLearnApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
