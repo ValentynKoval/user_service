625785d19eaf17c58a8fc8b2e88f19c645a0f779
Valentyn_Koval <J.K.play2015@gmail.com>
2026-01-18 19:07:07 +0200

    create UserRepositoryInMemory class with CRUD operations and data store configuration

---
+++ src/main/java/com/example/secondspringlearn/repository/UserRepositoryInMemory.java
@@ -0,0 +1,40 @@
+package com.example.secondspringlearn.repository;
+
+
+import com.example.secondspringlearn.model.User;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+@ConditionalOnProperty(name = "data.store.type", havingValue = "IN_MEMORY")
+public class UserRepositoryInMemory implements UserRepository {
+    private static final Map<Long, User> USERS = new HashMap<>();
+
+    public void add(User user) {
+        USERS.put(user.getId(), user);
+    }
+
+    public void update(User user) {
+        USERS.computeIfPresent(user.getId(), (key, value) -> user);
+    }
+
+    public void remove(long id) {
+        USERS.remove(id);
+    }
+
+    public User getById(long id) {
+        return USERS.get(id);
+    }
+
+    public User getByEmail(String email) {
+        return USERS.values().stream().filter(user -> user.getEmail().equals(email)).findFirst().orElse(null);
+    }
+
+    public Collection<User> getAll() {
+        return USERS.values();
+    }
+}
