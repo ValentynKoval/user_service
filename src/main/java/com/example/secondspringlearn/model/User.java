58ad84718d55984a83a6f891266e1c55b255841b
Valentyn_Koval <J.K.play2015@gmail.com>
2026-01-18 19:05:26 +0200

    create User entity with id, name, and email fields

---
+++ src/main/java/com/example/secondspringlearn/model/User.java
@@ -0,0 +1,24 @@
+package com.example.secondspringlearn.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
