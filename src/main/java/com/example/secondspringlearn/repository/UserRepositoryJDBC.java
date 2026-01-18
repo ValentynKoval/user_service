29ebb4365552792b029887a177b6e695b01ea65d
Valentyn_Koval <J.K.play2015@gmail.com>
2026-01-18 19:07:18 +0200

    create UserRepositoryJDBC implementation with JDBC data store support

---
+++ src/main/java/com/example/secondspringlearn/repository/UserRepositoryJDBC.java
@@ -0,0 +1,48 @@
+package com.example.secondspringlearn.repository;
+
+import com.example.secondspringlearn.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+@AllArgsConstructor
+@ConditionalOnProperty(name = "data.store.type", havingValue = "JDBC")
+public class UserRepositoryJDBC implements UserRepository{
+
+    private JdbcTemplate jdbcTemplate;
+
+    public void add(User user) {
+        String sql = "INSERT INTO user (name) VALUES (?)";
+        jdbcTemplate.update(sql, user.getName());
+    }
+
+    public User getById (long id) {
+        String sql = "SELECT * FROM user WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{id}, new BeanPropertyRowMapper<>(User.class));
+    }
+
+    public User getByEmail(String email) {
+        String sql = "SELECT * FROM user WHERE email = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{email}, new BeanPropertyRowMapper<>(User.class));
+    }
+
+    public Collection<User> getAll() {
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
+    }
+
+    public void update(User user) {
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, user.getName(), user.getId());
+    }
+
+    public void remove(long id) {
+        String sql = "DELETE FROM user WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+}
